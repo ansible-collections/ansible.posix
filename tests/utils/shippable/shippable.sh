91a6e30d7852d0a122d4b0b0ef1525cec9b2d754
Hideki Saito <saito@fgrep.org>
2023-04-12 15:52:02 +0900

    Update AZP to support stable-2.15 branch
    - Fixes #444
    
    Signed-off-by: Hideki Saito <saito@fgrep.org>

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -145,9 +145,9 @@ function cleanup
         fi
 
         if [ "${process_coverage}" ]; then
-            # use python 3.7 for coverage to avoid running out of memory during coverage xml processing
+            # use python 3.9 for coverage to avoid running out of memory during coverage xml processing
             # only use it for coverage to avoid the additional overhead of setting up a virtual environment for a potential no-op job
-            virtualenv --python /usr/bin/python3.7 ~/ansible-venv
+            virtualenv --python /usr/bin/python3.9 ~/ansible-venv
             set +ux
             . ~/ansible-venv/bin/activate
             set -ux
