af6b90981af6daa1a73e29fc5c2c4764cff6f480
Abhijeet Kasurde <akasurde@redhat.com>
2024-07-12 16:06:40 -0700

    Add Python version for Ubuntu and Fedora
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- tests/utils/shippable/linux.sh
+++ tests/utils/shippable/linux.sh
@@ -6,6 +6,15 @@ declare -a args
 IFS='/:' read -ra args <<< "$1"
 
 image="${args[1]}"
+pyver=default
+# check for explicit python version like 8.3@3.8
+declare -a splitversion
+IFS='@' read -ra splitversion <<< "$image"
+
+if [ "${#splitversion[@]}" -gt 1 ]; then
+    image="${splitversion[0]}"
+    pyver="${splitversion[1]}"
+fi
 
 if [ "${#args[@]}" -gt 2 ]; then
     target="shippable/posix/group${args[2]}/"
@@ -15,4 +24,4 @@ fi
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
-    --docker "${image}"
+    --docker "${image}" --python "${pyver}"
