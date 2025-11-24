e93424397d7713d16360fc238027922c9111c78b
Norman Ziegner <n.ziegner@hzdr.de>
2025-11-24 08:42:33 +0100

    Fix to_native deprecation warning in authorized_key module
    Signed-off-by: Norman Ziegner <n.ziegner@hzdr.de>

--- plugins/modules/authorized_key.py
+++ plugins/modules/authorized_key.py
@@ -227,7 +227,7 @@ import re
 import shlex
 from operator import itemgetter
 
-from ansible.module_utils._text import to_native
+from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils.six.moves.urllib.parse import urlparse
