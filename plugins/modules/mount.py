dda97be28a328f0b45df48ddbc107d550b2c8a5e
Pavel Bar <pbar@redhat.com>
2025-11-27 03:08:41 +0200

    Fix deprecated imports in plugins/modules/mount.py
    Replace deprecated module_utils imports:
    - ansible.module_utils._text -> ansible.module_utils.common.text.converters
    
    Fixes #686

--- plugins/modules/mount.py
+++ plugins/modules/mount.py
@@ -226,7 +226,7 @@ import platform
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.ansible.posix.plugins.module_utils.mount import ismount
 from ansible.module_utils.six import iteritems
-from ansible.module_utils._text import to_bytes, to_native
+from ansible.module_utils.common.text.converters import to_bytes, to_native
 from ansible.module_utils.parsing.convert_bool import boolean
 
 
